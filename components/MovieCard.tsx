d61aebe20475e819caab3b46fe77f39a61a3d096
HrishikeshUkarande1 <ukarandehrishikesh@gmail.com>
2024-06-17 19:14:49 +0530

    Resolved ESLint Warnings

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,25 +1,24 @@
 import React from "react";
-import { useRouter } from "next/router";    
+
+import { useRouter } from "next/router";
 
 import { BsFillPlayFill } from "react-icons/bs";
 import { BiChevronDown } from "react-icons/bi";
 import FavoriteButton from "./FavoriteButton";
 import useInfoModal from "@/hooks/useInfoModal";
 
-interface MovieCardProps{
-    data: Record<string, any>;
+interface MovieCardProps {
+  data: Record<string, any>;
 }
 
-const MovieCard: React.FC<MovieCardProps> = ({
-    data
-}) => {
-    const router = useRouter();
-    const { openModal } = useInfoModal();
+const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+  const router = useRouter();
+  const { openModal } = useInfoModal();
 
-    return (
-        <div className="group bg-zinc-900 col-span relative h-[12vw]">
-            <img 
-                className="
+  return (
+    <div className="group bg-zinc-900 col-span relative h-[12vw]">
+      <img
+        className="
                     cursor-pointer
                     object-cover
                     transition
@@ -32,9 +31,11 @@ const MovieCard: React.FC<MovieCardProps> = ({
                     w-full
                     h-[12vw]
                 "
-                src={data.thumbnailUrl} alt="Thumbnail" />
-                <div 
-                    className="
+        src={data.thumbnailUrl}
+        alt="Thumbnail"
+      />
+      <div
+        className="
                         opacity-0
                         absolute
                         top-0
@@ -50,10 +51,10 @@ const MovieCard: React.FC<MovieCardProps> = ({
                         group-hover:-translate-y-[6vw]
                         group-hover:translate-x-[2vw]
                         group-hover:opacity-100
-                    "    
-                >
-                    <img 
-                        className="
+                    "
+      >
+        <img
+          className="
                             cursor-pointer
                             object-cover
                             transition
@@ -63,9 +64,11 @@ const MovieCard: React.FC<MovieCardProps> = ({
                             w-full
                             h-[12vw]
                         "
-                    src={data.thumbnailUrl} alt="Thumbnail"/>
-                    <div
-                        className="
+          src={data.thumbnailUrl}
+          alt="Thumbnail"
+        />
+        <div
+          className="
                             z-10
                             bg-zinc-800
                             p-2
@@ -76,10 +79,10 @@ const MovieCard: React.FC<MovieCardProps> = ({
                             shadow-md
                             rounded-b-md
                         "
-                    >
-                        <div className="flex flex-row items-center gap-3">
-                            <div 
-                            className="
+        >
+          <div className="flex flex-row items-center gap-3">
+            <div
+              className="
                             cursor-pointer
                             w-6
                             h-6
@@ -93,13 +96,14 @@ const MovieCard: React.FC<MovieCardProps> = ({
                             transition
                             hover:bg-neutral-300
                             "
-                            onClick={() => router.push(`/watch/${data?.id}`) }>
-                                <BsFillPlayFill size={30} />
-                            </div>
-                            <FavoriteButton movieId={data?.id}/>
-                            <div 
-                                onClick={()=> openModal(data?.id)}
-                                className=" 
+              onClick={() => router.push(`/watch/${data?.id}`)}
+            >
+              <BsFillPlayFill size={30} />
+            </div>
+            <FavoriteButton movieId={data?.id} />
+            <div
+              onClick={() => openModal(data?.id)}
+              className=" 
                                     cursor-pointer 
                                     ml-auto 
                                     group/item 
@@ -113,25 +117,27 @@ const MovieCard: React.FC<MovieCardProps> = ({
                                     flex 
                                     justify-center 
                                     items-center 
-                                    transition hover:border-neutral-300">
-                                        <BiChevronDown 
-                                        size={30}
-                                        className="text-white group-hover/items:text-neutral-300"/>
-                            </div>
-                        </div>
-                        <p className="text-green-400 font-semibold mt-4">
-                            New <span className="text-white">2023</span>
-                        </p>
-                        <div className="flex flex-row mt-4 gap-2 items-center">
-                            <p className="text-white text-[10px] lg:text-sm">{data.duration}</p>
-                        </div>
-                        <div className="flex flex-row mt-4 gap-2 items-center">
-                            <p className="text-white text-[10px] lg:text-sm">{data.genre}</p>
-                        </div>                        
-                    </div>
-                </div>
+                                    transition hover:border-neutral-300"
+            >
+              <BiChevronDown
+                size={30}
+                className="text-white group-hover/items:text-neutral-300"
+              />
+            </div>
+          </div>
+          <p className="text-green-400 font-semibold mt-4">
+            New <span className="text-white">2023</span>
+          </p>
+          <div className="flex flex-row mt-4 gap-2 items-center">
+            <p className="text-white text-[10px] lg:text-sm">{data.duration}</p>
+          </div>
+          <div className="flex flex-row mt-4 gap-2 items-center">
+            <p className="text-white text-[10px] lg:text-sm">{data.genre}</p>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default MovieCard;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,37 +8,37 @@ import { useCallback, useEffect, useState } from "react";
 const TOP_OFFSET = 66; // height of the navbar + a bit extra for scrolling
 
 const Navbar = () => {
-    const [showMobileMenu, setShowMobileMenu] = useState(false);
-    const [showAccountMenu, setShowAccountMenu] = useState(false);
-    const [showBackground, setShowBackground] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY >= TOP_OFFSET) {
-                setShowBackground(true);
-            } else{
-                setShowBackground(false);
-            }
-        }
-        window.addEventListener("scroll", handleScroll);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= TOP_OFFSET) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        }
-    },[]);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    const toggleMobileMenu = useCallback(() => {
-        setShowMobileMenu((current) => !current);
-    },[]);
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((current) => !current);
+  }, []);
 
-    const toggleAccountMenu = useCallback(() => {
-        setShowAccountMenu((current) => !current);
-    },[]);
+  const toggleAccountMenu = useCallback(() => {
+    setShowAccountMenu((current) => !current);
+  }, []);
 
-    return(
-        <nav className="w-full fixed z-40">
-            <div
-                className={`
+  return (
+    <nav className="w-full fixed z-40">
+      <div
+        className={`
                     px-4
                     md:px-16
                     py-6
@@ -47,12 +47,12 @@ const Navbar = () => {
                     items-center
                     transition
                     duration-500
-                    ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}
+                    ${showBackground ? "bg-zinc-900 bg-opacity-90" : ""}
                 `}
-            >
-                <img className="h-5 lg:h-10" src="/images/logo.png"  alt="Logo" />
-                <div
-                    className="
+      >
+        <img className="h-5 lg:h-10" src="/images/logo.png" alt="Logo" />
+        <div
+          className="
                      flex
                      flex-row
                      ml-8
@@ -60,39 +60,52 @@ const Navbar = () => {
                      hidden
                      lg:flex
                     "
-                >
-                    <NavbarItem label="Home"/>
-                    <NavbarItem label="Series"/>
-                    <NavbarItem label="Movies"/>
-                    <NavbarItem label="New & Popular"/>
-                    <NavbarItem label="My List"/>
-                    <NavbarItem label="Browse by languages"/>
-                </div>
-                <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
-                    <p className="text-white text-sm">Browse</p>
-                    <BsChevronDown className={`text-white transition ${showMobileMenu ? 'rotate-180' : 'rotate-0' }`}/>
-                    <MobileMenu visible={showMobileMenu}/>
-                </div>
-                <div className="flex flex-row ml-auto gap-7 items-center">
-                    <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
-                        <BsSearch />
-                    </div>
-                    <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
-                        <BsBell />
-                    </div>
+        >
+          <NavbarItem label="Home" />
+          <NavbarItem label="Series" />
+          <NavbarItem label="Movies" />
+          <NavbarItem label="New & Popular" />
+          <NavbarItem label="My List" />
+          <NavbarItem label="Browse by languages" />
+        </div>
+        <div
+          onClick={toggleMobileMenu}
+          className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"
+        >
+          <p className="text-white text-sm">Browse</p>
+          <BsChevronDown
+            className={`text-white transition ${
+              showMobileMenu ? "rotate-180" : "rotate-0"
+            }`}
+          />
+          <MobileMenu visible={showMobileMenu} />
+        </div>
+        <div className="flex flex-row ml-auto gap-7 items-center">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <BsSearch />
+          </div>
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <BsBell />
+          </div>
 
-                    <div onClick={toggleAccountMenu} className="flex flex-row items-center gap-2 cursor-pointer relative">
-                        <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
-                            <img src="/images/Gojo.jpg" alt="" />
-                        </div>
-                        <BsChevronDown className={`text-white transition ${showAccountMenu ? 'rotate-180':'rotate-0'}`}/>
-                        <AccountMenu visible={showAccountMenu} />
-                    </div>
-
-                </div>
+          <div
+            onClick={toggleAccountMenu}
+            className="flex flex-row items-center gap-2 cursor-pointer relative"
+          >
+            <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+              <img src="/images/Gojo.jpg" width="100" height="100" alt="" />
             </div>
-        </nav>
-    )
-}
+            <BsChevronDown
+              className={`text-white transition ${
+                showAccountMenu ? "rotate-180" : "rotate-0"
+              }`}
+            />
+            <AccountMenu visible={showAccountMenu} />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
